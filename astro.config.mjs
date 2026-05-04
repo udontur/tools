1b3529bb581464ecee427e3365d29cf51e06eb3c
Hadrian <h@hadrian.cc>
2026-05-04 22:32:31 +0800

    feat: format and lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
+import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig, fontProviders } from "astro/config"
-import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
