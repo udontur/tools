3a3fccd1d33d93e80735079f3f86e3297aeb2d43
Hadrian <h@hadrian.cc>
2026-05-04 16:18:24 +0800

    feat: homepage structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
 
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
+import { defineConfig, fontProviders } from "astro/config"
 import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Manrope",
+        cssVariable: "--font-sans",
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "Space Grotesk",
+        cssVariable: "--font-heading",
+      },
+    ],
+    svgo: true,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
